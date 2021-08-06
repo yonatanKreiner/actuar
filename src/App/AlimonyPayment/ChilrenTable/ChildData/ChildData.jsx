3ab5924007217d75b4c99d4f00b1adfa30177ddd
ofirelarat <96ofir11@gmail.com>
2021-08-06 13:43:04 +0300

    add extra fields

--- src/App/AlimonyPayment/ChilrenTable/ChildData/ChildData.jsx
+++ src/App/AlimonyPayment/ChilrenTable/ChildData/ChildData.jsx
@@ -17,8 +17,28 @@ const ChildData = (props) => {
 
 	return (
 		<div className='container child-data-container-row'>
-			<input id={'datepicker'+props.index} className='datepicker' value={props.date} />
-			<input type='number' className="form-text amoutpicker" onChange={(e) => {props.handleSumChange(props.index, e.target.value)}} min='0' value={props.sum} />
+			<span className="data-row">
+				תאריך לידה: 
+				<input id={'datepicker'+props.index} className='datepicker' value={props.date} />
+			</span>
+			<span className="data-row">
+				סכום מזונות: 
+				<input type='number' className="form-text amoutpicker" onChange={(e) => {props.handleSumChange(props.index, e.target.value)}} min='0' value={props.sum} />
+			</span>
+			<span className="data-row">
+				אחוז שארית (מגיל 18):
+				<input type="number" step="0.01" className="form-text amoutpicker" />
+			</span>
+			<span className="data-row data-row-space-even">
+				<span className="custom-control custom-radio">
+					<input type="radio" className="custom-control-input" id="male" name="child-gender" checked />
+					<label className="custom-control-label" for="male">זכר</label>
+				</span>
+				<span className="custom-control custom-radio">
+					<input type="radio" className="custom-control-input" id="female"  name="child-gender"  />
+					<label className="custom-control-label" for="female">נקבה</label>
+				</span>
+			</span>
 		</div>
 	);
 }
