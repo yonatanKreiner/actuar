748fa1b2daac14d59e2ab169e82e427dbcc4070c
ofirelarat <96ofir11@gmail.com>
2022-12-01 09:09:12 +0200

    move to azure

--- src/App/config.js
+++ src/App/config.js
@@ -1,5 +1,5 @@
 export const GET_SERVER_URL = () => {
-    const apiUrl = process.env.NODE_ENV === 'production' ? 'https://actuar-calculator-interest.herokuapp.com': 'http://localhost:7000';
+    const apiUrl = process.env.NODE_ENV === 'production' ? 'https://actuarit-server.azurewebsites.net': 'http://localhost:7000';
 
     return apiUrl;
 } 
