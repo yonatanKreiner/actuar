c36ad4a31e06c1bb3ce497043e7b4e0008a0c4e1
ofirelarat <96ofir11@gmail.com>
2023-06-02 09:34:52 +0300

    update table data capabilities

--- src/App/AnnuitiesTable/AnnuitiesTable.jsx
+++ src/App/AnnuitiesTable/AnnuitiesTable.jsx
@@ -18,6 +18,31 @@ const AnnuitiesTable = (props) => {
         setAnnuities(result.result);
     }
 
+    const updateTableLocalState = (index, newRow) => {
+        const newAnnuities = [...annuities]
+        newAnnuities[index] = newRow;
+        setAnnuities(newAnnuities);
+    }
+
+    const addTableLocalStateRow = () => {
+        const newAnnuities = 
+            [...annuities, 
+                {
+                    year: new Date().getFullYear(),
+                    avgSalary: 0,
+                    annuityFreeFromTax: 0,
+                    maxCompensation: 0
+                }
+            ]
+        setAnnuities(newAnnuities);
+    }
+
+    const removeTableLocalStateRow = () => {
+        let newAnnuities = [...annuities]
+        newAnnuities = newAnnuities.slice(0, newAnnuities.length-1)
+        setAnnuities(newAnnuities);
+    }
+
     const updateTable = async (newRows) => {
         const apiUrl = `${GET_SERVER_URL()}/annuitiesTable`;
 		const response = await fetch(apiUrl,{
@@ -57,12 +82,40 @@ const AnnuitiesTable = (props) => {
     }
 
 	const generateRows = ((data) => {
-		return data.map(row => (
+		return data.map((row, index) => (
 		    <tr>
-                <td>{row.year}</td>
-                <td>{row.avgSalary}</td>
-                <td>{row.annuityFreeFromTax}</td>
-                <td>{row.maxCompensation}</td>
+                <td><input value={row.year} 
+                        onChange={(e) => updateTableLocalState(index, 
+                                                                {
+                                                                    year: e.target.value,
+                                                                    avgSalary: row.avgSalary,
+                                                                    annuityFreeFromTax: row.annuityFreeFromTax,
+                                                                    maxCompensation: row.maxCompensation
+                                                                })}/></td>
+                <td><input value={row.avgSalary}                      
+                        onChange={(e) => updateTableLocalState(index, 
+                                                                {
+                                                                    year: row.year,
+                                                                    avgSalary: e.target.value,
+                                                                    annuityFreeFromTax: row.annuityFreeFromTax,
+                                                                    maxCompensation: row.maxCompensation
+                                                                })}/></td>
+                <td><input value={row.annuityFreeFromTax}
+                        onChange={(e) => updateTableLocalState(index, 
+                                                                {
+                                                                    year: row.year,
+                                                                    avgSalary: row.avgSalary,
+                                                                    annuityFreeFromTax: e.target.value,
+                                                                    maxCompensation: row.maxCompensation
+                                                                })}/></td>
+                <td><input value={row.maxCompensation} 
+                        onChange={(e) => updateTableLocalState(index, 
+                                                                {
+                                                                    year: row.year,
+                                                                    avgSalary: row.avgSalary,
+                                                                    annuityFreeFromTax: row.annuityFreeFromTax,
+                                                                    maxCompensation: e.target.value
+                                                                })}/></td>
             </tr>
 		));
 	});
@@ -74,6 +127,9 @@ const AnnuitiesTable = (props) => {
                 {generateTable(annuities)}
             </div>
             <CsvReader importRows={updateTable}></CsvReader>
+            <button className='btn btn-outline-info' onClick={()=>updateTable(annuities)}>שמור שינויים</button>
+            <button className='btn btn-outline-warning' onClick={addTableLocalStateRow}>הוסף שורה</button>
+            <button className='btn btn-outline-warning' onClick={removeTableLocalStateRow}>מחק שורה</button>
 		</div>
 	);
 }
