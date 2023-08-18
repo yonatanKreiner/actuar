9e918010d436046b923a63d71e4b6dfa7e00f578
ofirelarat <96ofir11@gmail.com>
2023-08-18 19:21:50 +0300

    added insert and remove btns functionality

--- src/App/AnnuityCalculator/PartTwoTablePayload/PartTwoTablePayload.jsx
+++ src/App/AnnuityCalculator/PartTwoTablePayload/PartTwoTablePayload.jsx
@@ -7,43 +7,89 @@ import {
     AccordionItemButton,
     AccordionItemPanel,
 } from 'react-accessible-accordion';
+import { useState } from 'react';
 
 const PartTwoTablePayload = () => {
 
-    return (
-        <div>
-            <Accordion>
-                <AccordionItem>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            <table style={{direction:"rtl"}} className="table">
-                                <thead>
-                                    <th>קופה</th>
-                                    <th>מס תוכנית/פוליסה</th>
-                                    <th>סוג</th>
-                                    <th>מדרג</th>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <input/>
-                                        </td>
-                                        <td>
-                                            <input/>
-                                        </td>
-                                        <td>
-                                            <input/>
-                                        </td>
-                                        <td>
-                                            <input/>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                       <table  style={{direction:"rtl"}} className="table">
+    const [policiesTable, setPoliciesTable] = useState([]);
+
+    const insertPolicy = () => {
+        const newPolicy = {
+            name: '',
+            id: '',
+            type: '',
+            order: policiesTable.length,
+            deposits: []
+        }
+
+        setPoliciesTable([...policiesTable, newPolicy]);
+    }
+
+    const removePolicy = () => {
+        let newPolicies = [...policiesTable]
+        newPolicies = newPolicies.slice(0, newPolicies.length - 1)
+        setPoliciesTable(newPolicies);
+    }
+
+    const addDeposit = () => {
+
+    }
+
+    const generateDeposits = (policy, index) => {
+        return (
+            policy.deposits.map(d => (
+                <tr>
+                    <td>
+                        <input value={d.year} type='number' />
+                    </td>
+                    <td>
+                        <input value={d.depositeEmpoloyee} />
+                    </td>
+                    <td>
+                        <input value={d.depositeCompany} />
+                    </td>
+                    <td>
+                        <input value={d.depositeCompensation} />
+                    </td>
+                </tr>)
+            )
+        )
+    }
+
+    const generatePolicies = () => {
+        return policiesTable.map((p, index) => {
+            return (<AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <table style={{ direction: "rtl" }} className="table">
+                            <thead>
+                                <th>קופה</th>
+                                <th>מס תוכנית/פוליסה</th>
+                                <th>סוג</th>
+                                <th>מדרג</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <input value={p.name} />
+                                    </td>
+                                    <td>
+                                        <input value={p.id} />
+                                    </td>
+                                    <td>
+                                        <input value={p.type} />
+                                    </td>
+                                    <td>
+                                        <input value={p.order} type='number' />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <div>
+                        <table style={{ direction: "rtl" }} className="table">
                             <thead className="thead-light">
                                 <th>שנת מס</th>
                                 <th>עובד</th>
@@ -51,26 +97,41 @@ const PartTwoTablePayload = () => {
                                 <th>פיצויים</th>
                             </thead>
                             <tbody>
-                                
+                                {generateDeposits(p, index)}
                             </tbody>
-                       </table>
-                    </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            Is free will real or just an illusion?
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                        <p>
-                            In ad velit in ex nostrud dolore cupidatat consectetur
-                            ea in ut nostrud velit in irure cillum tempor laboris
-                            sed adipisicing eu esse duis nulla non.
-                        </p>
-                    </AccordionItemPanel>
-                </AccordionItem>
+                        </table>
+                        <button className='btn btn-outline-warning' onClick={() => {
+                            const newPolicies = [...policiesTable];
+                            newPolicies[index].deposits.push({
+                                year: 0,
+                                depositeEmpoloyee: 0,
+                                depositeCompany: 0,
+                                depositeCompensation: 0
+                            })
+
+                            setPoliciesTable(newPolicies);
+                        }}>
+                            הוסף שורה</button>
+                        <button className='btn btn-outline-warning' onClick={() => {
+                            let newPolicies = [...policiesTable]
+                            const newDeposits = newPolicies[index].deposits.slice(0, newPolicies[index].deposits.length - 1)
+                            newPolicies[index].deposits = newDeposits;
+                            setPoliciesTable(newPolicies);
+                        }}>מחק שורה</button>
+                    </div>
+                </AccordionItemPanel>
+            </AccordionItem>)
+        })
+    }
+
+    return (
+        <div>
+            <Accordion>
+                {generatePolicies()}
             </Accordion>
+
+            <button className='btn btn-outline-warning' onClick={insertPolicy}>הוסף קופה</button>
+            <button className='btn btn-outline-warning' onClick={removePolicy}>מחק קופה</button>
         </div>
     );
 }
