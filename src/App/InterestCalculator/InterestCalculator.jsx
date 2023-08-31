08fd29519508fe0b8d81a5b757b7fbcdac5f2788
ofirelarat <96ofir11@gmail.com>
2023-08-31 16:00:49 +0300

    fix promise all problem

--- src/App/InterestCalculator/InterestCalculator.jsx
+++ src/App/InterestCalculator/InterestCalculator.jsx
@@ -1,6 +1,6 @@
 import './InterestCalculator.css';
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { jsPDF } from "jspdf";
 import html2canvas from "html2canvas";
 import { useSnackbar } from 'react-simple-snackbar'
@@ -13,17 +13,17 @@ import ResultItem from './ResultItem';
 const InterestCalculator = () => {
 
 	const [debts, setDebts] = useState([
-		{startDate: new Date(), sum: 100, interestType: 'legal-interest', endDate: new Date()}
+		{ startDate: new Date(), sum: 100, interestType: 'legal-interest', endDate: new Date() }
 	]);
 
 	const [openSnackbar] = useSnackbar()
 
-	useEffect(()=> {
-		if(window.location.search.substr(1).includes("combackCalc")){
+	useEffect(() => {
+		if (window.location.search.substr(1).includes("combackCalc")) {
 			const payload = window.sessionStorage.getItem("interestCalculationImportPayload");
-			const payloadData = JSON.parse(payload); 
+			const payloadData = JSON.parse(payload);
 			setDebts(payloadData);
-		}else {
+		} else {
 			loadPayloadFromSessionStorage();
 		}
 	}, [])
@@ -35,8 +35,8 @@ const InterestCalculator = () => {
 	const handleChangeDebt = (index, debt) => {
 		console.log(debt);
 		setDebts([
-			...debts.slice(0, index), 
-			debt, 
+			...debts.slice(0, index),
+			debt,
 			...debts.slice(index + 1)
 		]);
 	}
@@ -54,27 +54,34 @@ const InterestCalculator = () => {
 	}
 
 	const handleCalculate = async () => {
-		try{
-			const resultsPromise = [];
+		try {
+			let resultsPromise = [];
 			const CHUNK = 2;
-			for(let i=0; i < debts.length; i += CHUNK) {
+			const results_arrays = [];
+
+			for (let i = 0; i < debts.length; i += CHUNK) {
 				const debtsChunk = debts.slice(i, i + CHUNK);
 
-				const requestData = { debts: debtsChunk };	
+				const requestData = { debts: debtsChunk };
 				const chunkResultPromise = calcDepts(requestData);
 				resultsPromise.push(chunkResultPromise);
+
+				if (resultsPromise.length % 3 == 0 || i+CHUNK >= debts.length) {
+					const results_arrays_promise = Promise.all(resultsPromise);
+					const resultsData = await results_arrays_promise
+					results_arrays.push(resultsData.flat());
+					resultsPromise = [];
+				}
 			}
 
-			const results_arrays_promise = Promise.all(resultsPromise);
-			const results_arrays = await results_arrays_promise;
 			const results = results_arrays.flat()
 
-			const totalDebt = results.reduce((total, debtResult) => total + parseFloat(debtResult.totalDebt.replace(',','')), 0);
+			const totalDebt = results.reduce((total, debtResult) => total + parseFloat(debtResult.totalDebt.replace(',', '')), 0);
 			setDebts([...results]);
 			savePayloadToSessionStorage([...results]);
 
-			return {allDepts: results, total: totalDebt.toLocaleString(undefined,{ minimumFractionDigits: 2 })};
-		} catch(err) {
+			return { allDepts: results, total: totalDebt.toLocaleString(undefined, { minimumFractionDigits: 2 }) };
+		} catch (err) {
 			openSnackbar('החישוב נכשל, אנא נסה שנית או פנה לגורם טכני')
 		}
 	}
@@ -82,21 +89,21 @@ const InterestCalculator = () => {
 	const calcDepts = async (requestData) => {
 		const apiUrl = `${GET_SERVER_URL()}`;
 
-		const response = await fetch(apiUrl,{
+		const response = await fetch(apiUrl, {
 			method: 'post',
-			headers: {"Content-Type": "application/json"},
+			headers: { "Content-Type": "application/json" },
 			credentials: "include",
 			body: JSON.stringify(requestData)
 		});
 
 		const data = await response.json();
 		const results = data.finalDebt;
-		results.allDepts = results.allDepts.map(deptResult => { 
+		results.allDepts = results.allDepts.map(deptResult => {
 			return {
 				...deptResult,
 				interestTypetext: deptResult.interestType === 'legal-interest' ? "ריבית צמודה" :
-									 deptResult.interestType === 'illegal-interest' ? "ריבית פיגורים" :
-									 deptResult.interestType === 'shekel-interest' ? "ריבית שקלית" : "הצמדה למדד"
+					deptResult.interestType === 'illegal-interest' ? "ריבית פיגורים" :
+						deptResult.interestType === 'shekel-interest' ? "ריבית שקלית" : "הצמדה למדד"
 			}
 		});
 
@@ -112,15 +119,15 @@ const InterestCalculator = () => {
 
 		const pdf = new jsPDF("p", "mm", "a4");
 
-		const imgProps= pdf.getImageProperties(resultsImgData);
+		const imgProps = pdf.getImageProperties(resultsImgData);
 		const width = pdf.internal.pageSize.getWidth();
 		const height = (imgProps.height * width) / imgProps.width;
-		
-		const pageHeight =  295;  
+
+		const pageHeight = 295;
 		let heightLeft = height;
 		let position = 30;
 
-		pdf.addImage(headerImgData, 'JPEG', -0.55*width, 5);
+		pdf.addImage(headerImgData, 'JPEG', -0.55 * width, 5);
 		pdf.addImage(resultsImgData, 'JPEG', 2, 30, width - 4, height);
 		heightLeft -= pageHeight;
 
@@ -134,34 +141,34 @@ const InterestCalculator = () => {
 		pdf.save("חישוב פסיקת ריבית.pdf");
 	}
 
-	const savePayloadToSessionStorage =  (debtsPayload) => {
+	const savePayloadToSessionStorage = (debtsPayload) => {
 		const interestsPayload = {
 			debts: debtsPayload
 		};
-		
+
 		window.sessionStorage.setItem('interestPamentPayload', JSON.stringify(interestsPayload));
 	}
 
 	const loadPayloadFromSessionStorage = () => {
 		const payload = window.sessionStorage.getItem("interestPamentPayload");
-		if(payload){
-			const payloadData = JSON.parse(payload); 
+		if (payload) {
+			const payloadData = JSON.parse(payload);
 			setDebts(payloadData.debts);
 		}
 	}
 
 	return (
 		<div className='containter centered'>
-			<Header/>
-			<hr/>
-			<DebtsTable 
+			<Header />
+			<hr />
+			<DebtsTable
 				importDebts={handleImportDebts}
 				handleChangeDebt={handleChangeDebt}
-				addDebt={handleAddDebt} 
-				removeDebt={handleRemoveDebt} 
+				addDebt={handleAddDebt}
+				removeDebt={handleRemoveDebt}
 				removeAllDepts={handleRemoveAllDebt}
 				debts={debts} />
-			<hr/>
+			<hr />
 			<ResultItem calculateDept={handleCalculate} generatePDF={generatePDF} />
 		</div>
 	);
